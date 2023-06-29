be9b3a5fd7c10514f3126a3cf3e661983cb298ed
TanyaMosk <123247337+TanyaMosk@users.noreply.github.com>
2023-06-29 15:59:21 +0000

    Deploying to gh-pages from @ TanyaMosk/goit-js-hw-10@540cda97bf054572a97228d6ff8710e9e75c96db 🚀

--- index.0d3c3cf1.js
+++
@@ -1,2 +0,0 @@
-fetch("https://api.thecatapi.com/v1/breeds").then((function(t){return t.json})).then(console.log);
-//# sourceMappingURL=index.0d3c3cf1.js.map
--- index.c0171f56.js
+++
@@ -1,2 +0,0 @@
-fetch("https://api.thecatapi.com/v1/breeds").then((e=>e.json)).then(console.log);
-//# sourceMappingURL=index.c0171f56.js.map
