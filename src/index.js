fcd1e871936424fe6fccea2af43b7e51734167f2
TanyaMosk <tanya856227@gmail.com>
2023-06-30 17:27:35 +0200

    index + styles

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './styles.css';
 import { fetchBreeds, fetchCatByBreed } from './cat-api';
+import Notiflix from 'notiflix';
 import SlimSelect from 'slim-select';
 import 'slim-select/dist/slimselect.css';
 
@@ -9,6 +10,30 @@ const refs = {
   loader: document.querySelector('.loader'),
   ifError: document.querySelector('.error'),
 };
+
 refs.breedSelect.addEventListener('change', onChooseCatBreed);
+
+refs.loader.classList.remove('is-hidden');
+
+let catsBreedsId = [];
+fetchBreeds()
+  .then(data => {
+  //   for (let i = 0; i < data.length; i += 1) {
+  //     const cat = data[i];
+  //   catsBreedsId.push({ text: cat.name, value: cat.id });
+  // }
+  data.forEach(cat => {
+      catsBreedsId.push({ text: cat.name, value: cat.id });
+    });
+    new SlimSelect({
+      select: refs.breedSelect,
+      data: catsBreedsId,
+      settings: {
+        showOptionTooltips: true,
+      },
+    });
+  })
+  .catch(onError);
+
 
 
