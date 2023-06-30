9c1f950b410a7e8e5a853eab8473027c3b749870
TanyaMosk <tanya856227@gmail.com>
2023-06-30 21:31:20 +0200

    index, styles

--- src/index.js
+++ src/index.js
@@ -35,5 +35,50 @@ fetchBreeds()
   })
   .catch(onError);
 
+function onChooseCatBreed(event) {
+  refs.catInfo.innerHTML = '';
+  refs.loader.classList.remove('is-hidden');
 
+  const breedId = event.currentTarget.value;
+
+  fetchCatByBreed(breedId)
+    .then(data => {
+      refs.loader.classList.add('is-hidden');
+      // console.log(breedId);
+      createMarkup(data);
+    })
+    .catch(onError);
+}
+
+function createMarkup(cats) {
+  return cats
+    .map(({ url, breeds }) => {
+      // const { url, breeds } = cat;
+      refs.catInfo.innerHTML = /*html*/ `
+    <div class="cat-description">
+      <img src="${url}" alt="${breeds[0].name}" width="460">
+      <div class="cat-text">
+      <h2>${breeds[0].name}</h2>
+      <h3>Origin</h3>
+      <p>${breeds[0].origin}</p>
+      <h3>Temperament</h3>
+      <p>${breeds[0].temperament}</p>
+      <h3>Description</h3>
+      <p>${breeds[0].description}</p></div>
+    </div> `;
+    })
+    .join('');
+}
+
+function onError() {
+  refs.loader.classList.remove('is-hidden');
+
+  Notiflix.Notify.failure(refs.ifError.textContent, {
+    position: 'center-top',
+    timeout: 5000,
+    width: '520px',
+    fontSize: '20px',
+    cssAnimationStyle: 'from-top',
+  });
+}
 
