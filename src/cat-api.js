da52810766dc7d77972d0fd65e1bf06f28a1c8c0
TanyaMosk <tanya856227@gmail.com>
2023-06-30 17:22:26 +0200

    cat-api

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,24 +1,26 @@
 const BASE_URL = 'https://api.thecatapi.com/v1/';
-const END_POINT = './breeds';
-const KEY =
+const QUERY_PARAMETERS = './breeds';
+const API_KEY =
   'live_kne6mpkXLgm7nUZCS4ggJbVB6nrlBRkssF2XTvE8EvJdlnobGv1bcarHfgDxjrmo';
 
 export function fetchBreeds() {
-  return fetch(`${BASE_URL}${END_POINT}?api_key=${KEY}`).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }    
-    return response.json();
-  });
+  return fetch(`${BASE_URL}${QUERY_PARAMETERS}?api_key=${API_KEY}`).then(
+    response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    }
+  );
 }
 
 export function fetchCatByBreed(breedId) {
   return fetch(
-    `${BASE_URL}images/search?breed_ids=${breedId}&api_key=${KEY}`
+    `${BASE_URL}images/search?breed_ids=${breedId}&api_key=${API_KEY}`
   ).then(response => {
     if (!response.ok) {
       throw new Error(response.status);
     }
-    return response.json();    
+    return response.json();
   });
 }
